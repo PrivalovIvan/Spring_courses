de33e0b0b6184f8a2b0eb911588e16a8059fdb96
Иван Привалов <mamamia89678524930@gmail.com>
2023-08-20 21:10:24 +0500

    Выражения pointcut.

--- lesson/src/test/java/aop/Test1.java
+++ lesson/src/test/java/aop/Test1.java
@@ -5,8 +5,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        Library lib = context.getBean("libraryBean", Library.class);
-        lib.getBook();
+        UniLibrary uniLib = context.getBean("uniLibrary", UniLibrary.class);
+        SchoolLibrary sLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
+        uniLib.getBook();
+        uniLib.returnBook();
+        uniLib.getMagazine();
+
+        sLibrary.getBook();
+
         context.close();
     }
 }
--- lesson/src/test/java/aop/aspects/LoggingAspect.java
+++ lesson/src/test/java/aop/aspects/LoggingAspect.java
@@ -7,9 +7,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LoggingAspect {
-    @Before("execution(public void getBook())")
+    // @Before("execution(public void get*())")
     public void beforeGetBookAdvace() {
-        System.out.println("beforeGetBookAdvace: попытка получить книгу.");
+        System.out.println("beforeGetBookAdvace: попытка получить книгу.\n");
+    }
+
+    // @Before("execution(public void return*())")
+    public void beforeReturnBookAdvace() {
+        System.out.println("beforeReturnBookAdvace: попытка вернуть книгу.\n");
+    }
+
+    @Before("execution(* *())")
+    public void beforeForever() {
+        System.out.println("Я тут\n");
     }
 
 }
