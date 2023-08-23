1b958ffbe0e8efda813f0c4f83cb78a9026e18df
Иван Привалов <mamamia89678524930@gmail.com>
2023-08-23 16:37:24 +0500

    Advice тип @Around.Работа с исключениями.

--- lesson/src/test/java/aop/aspects/NewLoggingAspect.java
+++ lesson/src/test/java/aop/aspects/NewLoggingAspect.java
@@ -11,7 +11,14 @@ public class NewLoggingAspect {
     @Around("execution(public String returnBook())")
     private Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint point) throws Throwable {
         System.out.println("aroundReturnBookLoggingAdvice: в библиотеку пытаются вернуть книгу.");
-        Object targetMethodResult = point.proceed();
+        Object targetMethodResult = null;
+        try {
+            targetMethodResult = point.proceed();
+
+        } catch (Exception e) {
+            System.out.println("Исключение " + e + " было залогировано.");
+            throw e;
+        }
         System.out.println("aroundReturnBookLoggingAdvice: в библиотеку вернули книгу.");
         return targetMethodResult;
     }
